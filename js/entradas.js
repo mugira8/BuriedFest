5d9d2a99c3ed3332522c7a0efb6433b4c4c7c451
mugira8 <74660528+mugira8@users.noreply.github.com>
2021-09-29 12:41:12 +0200

    Completo

--- js/entradas.js
+++ js/entradas.js
@@ -11,9 +11,13 @@ function comprar($num, $tipo) {
         case 2:
             $texto = "5 DAY TICKET"; 
     }
-    if( $disponibles[$tipo]<$num.value){
-        alert("Oh no, sentimos comunicarle que no disponemos de ese numero de entradas :( \n UwU")
-    }else{
+    if( $disponibles[$tipo]<$num.value && $disponibles[$tipo]!=0){
+        if(confirm("Oh no, sentimos comunicarle que no disponemos de ese numero de entradas :( \n UwU \n ¿Quieres comprar las entradas restantes?")){
+            $disponibles[$tipo]=0;
+            document.getElementsByClassName('dispon')[$tipo].innerHTML = "  <h5>Total entradas: "+$total[$tipo]+"</h5><h5>Entradas disponibles: "+$disponibles[$tipo]+"</h5>";
+            document.getElementsByClassName('btnCompra')[$tipo].innerHTML = "AGOTADO";
+        }
+    }else if ($disponibles[$tipo]>$num.value){
         alert($texto + "\n Has comprado " + $num.value + " entrada/entradas.");
         $disponibles[$tipo] = $disponibles[$tipo] - $num.value;
         document.getElementsByClassName('dispon')[$tipo].innerHTML = "  <h5>Total entradas: "+$total[$tipo]+"</h5><h5>Entradas disponibles: "+$disponibles[$tipo]+"</h5>";
